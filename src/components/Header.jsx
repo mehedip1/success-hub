77edab9e7320b56b7771d77606426f4042f2795b
mehedip1 <Promehedi30@gmail.com>
2023-04-10 10:31:19 +0600

    text added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <div className='ms-auto  mx-auto p-5 lg:mt-5 bg-gradient-to-r from-sky-300 to-indigo-700 font-bold'>
+        <div className='ms-auto  mx-auto p-5 lg:mt-5 bg-gradient-to-r from-sky-300 to-indigo-300 font-bold'>
             <div className='py-5 ms-5 grid grid-cols-3 gap-4'>
             <h1 className='text-5xl ms-5 text-bold'>SuccessHub</h1>
               <div className='px-5 py-5 ms-5'>
@@ -12,7 +12,7 @@ const Header = () => {
              <Link className='px-5' to='/Jobs'>Jobs</Link>
              <Link className='px-5' to='/Blog'>Blog</Link>
               </div>
-              <button className='rounded ms-5 w-2/4 text-white bg-indigo-500'>Star Applying</button>
+              <button className='rounded-full ms-5 px-5 mx-5 w-2/4 text-white bg-indigo-500'>Star Applying</button>
            </div>
         </div>
     );
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
+import logo from '.././components/images/logo.png'
 
-
+ 
 
 const Home = () => {
     return (
-        <div>
-          
+        <div className='grid grid-cols-2 gap-5 mt-5'>
+          <div>
+          <h1 className='text-5xl mt-15 mb-10 ms-5 font-bold py-10 my-10'>
+          One Step Closer To Your <br /><span className='text-cyan-400'>Dream Job</span>
+         </h1>
+         <p className='ms-5'>Explore thousands of job opportunities with all the information you need. Its your future. Come find it. Manage all your job application from start to finish.</p>
+        <div className='py-5 ms-5'>
+        <button  className='text-white  ms-5 px-5 mx-5 w-2/4 h-100% bg-indigo-500 rounded'>Get Started</button>
+        </div>
+        
+         
+          </div>
+          <img src={logo}  alt="" />
         </div>
     );
 };
