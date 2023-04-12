5d422ef76e98e91ea7d756621bf7e4e5e06eac64
mehedip1 <Promehedi30@gmail.com>
2023-04-12 06:08:16 +0600

    blog text added

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -2,8 +2,12 @@ import React from 'react';
 
 const Blog = () => {
     return (
-        <div>
-            <h1>Blogs here</h1>
+        <div className='mt-5'>
+            <h1>Context Api: When your state needs to be accessed or set from many child components</h1>
+            <br />
+            <p>Custom Hooks: When you have component logic that needs to be used by multiple components, we can extract that logic to a custom Hook.</p>
+            <br />
+            <p>The useRef Hook can also be used to keep track of previous state values. This is because we are able to persist useRef values between renders.</p>
         </div>
     );
 };
